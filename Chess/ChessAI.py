ec2c752f06bc2dcecce3dcfc9d5523939189f4cc
diamondoladeji <msdee200470@yahoo.com>
2026-03-31 14:28:52 -0500

    ran ai vs ai tests; also modded main and chessAI

---
+++ Chess/ChessAI.py
@@ -0,0 +1,6 @@
+import random 
+
+def findRandomMove(validMoves):
+    """Returns a random valid move, used for(easy)random move AI"""
+
+    return random.choice(validMoves)
--- main.py
+++ main.py
@@ -1,3 +1,15 @@
+import pygame as p
+from Chess import ChessEngine, ChessAI
+
+WIDTH = HEIGHT = 512
+DIMENSION = 8
+SQ_SIZE = HEIGHT // DIMENSION
+MAX_FPS = 15
+IMAGES = {}
+
+PLAYER_VS_AI = 1
+gameMode = PLAYER_VS_AI
+
 def main():
     print("Hello, World!")
 
